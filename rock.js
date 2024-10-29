1423a202fc0feccd3022bfa45eefe434de756b60
Holly <162161116+Holl4444@users.noreply.github.com>
2024-10-29 12:03:13 +0000

    Add contingency for several invalid entries
    Added a catch for multiple invalid entries from
    the user to prevent the game running indefinitely.
    
    Added comments throughout.
     Changes to be committed:
    	modified:   rock.js

--- rock.js
+++ rock.js
@@ -2,28 +2,40 @@ const RPS_MAX = 3;
 const CHOICE_OPTIONS = ["rock", "paper", "scissors"];
 
 function getComputerChoice(max, array) {
-  /* Get a random number between 0 and 2*/
+  // Get a random number between 0 and 2
   let C_choice = Math.floor(Math.random() * RPS_MAX);
-  /* Initialise array with game choice options */
-
+  // return what we find at this index of the array
   return array[C_choice];
 }
 console.log(getComputerChoice(RPS_MAX, CHOICE_OPTIONS));
 
 function getHumanChoice() {
+  //ask user for choice input and cover for differences in case
   let H_choice = prompt("rock, paper or scissors? ").toLowerCase();
+  //Set up a variable to stop unlimited invalid entries
+  let invalidEntries = 0;
+  // if the user input isnt valid alert and ask again until valid or
+  // there have been more than 3 invalid attempts
   while (
     H_choice !== "rock" &&
     H_choice !== "paper" &&
     H_choice !== "scissors"
   ) {
-    alert(`Sorry "${H_choice}" is not a valid option.`);
-    H_choice = prompt("rock, paper or scissors? ").toLowerCase();
+    invalidEntries++;
+    if (invalidEntries > 3) {
+      alert("Too many invalid entries, ending programme.");
+      break;
+    } else {
+      alert(`Sorry "${H_choice}" is not a valid option.`);
+      H_choice = prompt("rock, paper or scissors? ").toLowerCase();
+    }
   }
+  // All being well return valid choice
   return H_choice;
 }
 console.log(getHumanChoice());
 
+// initiate variables for scores and rounds
 let computerScore = 0;
 let humanScore = 0;
 let totalRounds = 5;
